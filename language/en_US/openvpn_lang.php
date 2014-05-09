00873d7d2f0fddde02bdc84b8baee15856ac4a1b
pcbaldwin <pbaldwin@clearfoundation.com>
2014-05-08 20:42:47 -0400

    Updated translations

--- language/en_US/openvpn_lang.php
+++ language/en_US/openvpn_lang.php
@@ -2,11 +2,8 @@
 
 $lang['openvpn_app_description'] = 'The OpenVPN app provides secure remote access to this system and your local network.';
 $lang['openvpn_app_name'] = 'OpenVPN';
-$lang['openvpn_domain'] = 'Domain';
-$lang['openvpn_dns_server'] = 'DNS Server';
-$lang['openvpn_wins_server'] = 'WINS Server';
 $lang['openvpn_auto_configure_help'] = 'The OpenVPN server is currently in auto-configuration mode.  If you change your network settings, OpenVPN will intelligently make the necessary changes to its settings.  If you would like to disable auto-configuration, please click on the following button.';
-$lang['openvpn_windows'] = 'Windows';
+$lang['openvpn_internet_hostname_tooltip'] = 'Then Internet Hostname is used in the OpenVPN client configuration provided to end users.  This hostname can be changed in the IP Settings app.';
 $lang['openvpn_linux'] = 'Linux';
 $lang['openvpn_mac_os'] = 'MacOS';
-$lang['openvpn_internet_hostname_tooltip'] = 'Then Internet Hostname is used in the OpenVPN client configuration provided to end users.  This hostname can be changed in the IP Settings app.';
+$lang['openvpn_windows'] = 'Windows';
