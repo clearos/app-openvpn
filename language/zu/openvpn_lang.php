754c4b5b38e3cbf6b7698dcee0bae826afaf9851
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:17:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Uhlelo lokusebenza OpenVPN inikeza elondekile ukungena kokukude lesi simiso futhi inethiwekhi wangakini.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Iseva ye-OpenVPN okwamanje auto-ukumisa imodi. Uma ushintsha izilungiselelo zenethiwekhi yakho, OpenVPN ngeke ngokukhalipha wenze izinguquko ezidingekayo ukuze kuzilungiselelo zalo. Uma ungathanda ukuba ukukhubaza auto-ukumisa, sicela uchofoze inkinobho elandelayo.';
+$lang['openvpn_internet_hostname_tooltip'] = 'I-Internet Igama lomethuleli isetshenziswa OpenVPN iklayenti ukucushwa enikeziwe kubasebenzisi ekupheleni. Lokhu Igama lomethuleli kungashintshwa kuhlelo lokusebenza lezilungiselelo IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
