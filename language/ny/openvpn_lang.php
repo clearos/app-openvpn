8cc90cb4359f5f925b725152dceb10c9120b380b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:07:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'The app OpenVPN amapereka otetezeka mwayi kutali dongosolo ndi maukonde wanu.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Seva OpenVPN panopa mu mode galimoto-kasinthidwe. Ngati inu sinthani maukonde anu, OpenVPN adzakhala mwanzeru kusintha kwa zoikamo ake. Ngati mukufuna kuletsa galimoto-kasinthidwe, chonde alemba pa batani zotsatirazi.';
+$lang['openvpn_internet_hostname_tooltip'] = 'The Hostname Internet ntchito kasitomala kasinthidwe OpenVPN anapereka kwa owerenga mapeto. hostname izi zikhoza kusintha mu app IP Zikhazikiko.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
