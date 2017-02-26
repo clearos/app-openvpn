3b092ff392adbed86a2266d1df5ff1f379ce33e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:09:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'The OpenVPN app bayar amintacce m damar yin amfani da wannan tsarin da kuma gida na cibiyar sadarwa.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'The OpenVPN uwar garken ne a halin yanzu a auto-sanyi mode. Idan ka canza hanyar sadarwa saituna, OpenVPN za intelligently yi zama dole canje-canje zuwa da saituna. Idan ka so ka musaki auto-sanyi, don Allah danna kan wadannan button.';
+$lang['openvpn_internet_hostname_tooltip'] = 'The Internet Sunan mai masauki da ake amfani a cikin OpenVPN abokin ciniki sanyi ya samar karshen users. Wannan sunan mai masauki za a iya canja a cikin IP Saituna app.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
