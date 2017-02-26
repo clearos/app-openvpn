0d71bc06c64a5029716fd051aafb1c536d85d046
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:05:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Aplikacioni OpenVPN siguron qasje të sigurt të largët në këtë sistem dhe në rrjetin tuaj lokal.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Server OpenVPN është aktualisht në mënyrë auto-konfigurimit. Nëse ju të ndryshojë konfigurimin e rrjetit tuaj, OpenVPN inteligjente do të bëjë ndryshimet e nevojshme në parametrat e saj. Nëse dëshironi të çaktivizoni auto-konfigurimin, ju lutemi klikoni në butonin e mëposhtëm.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Interneti Hostname është përdorur në konfigurimin e klientit OpenVPN të dhënë për përdoruesit përfundimtarë. Kjo hostname mund të ndryshohet në aplikacionin IP Settings.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
