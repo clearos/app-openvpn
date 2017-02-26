f986b8d161ef5ef13450f31110121351c8edd544
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:10:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPNのアプリは、このシステムとローカルネットワークへの安全なリモートアクセスを提供します。';
+$lang['openvpn_app_name'] = 'OpenVPNの';
+$lang['openvpn_auto_configure_help'] = 'OpenVPNサーバーは、自動設定モードに現在あります。 あなたのネットワーク設定を変更する場合は、OpenVPNのは、インテリジェントにその設定に必要な変更を行います。 あなたは自動設定を無効にしたい場合は、以下のボタンをクリックしてください。';
+$lang['openvpn_internet_hostname_tooltip'] = 'インターネットホスト名は、エンドユーザに提供OpenVPNのクライアント構成で使用されています。 このホスト名は、IP設定アプリで変更することができます。';
+$lang['openvpn_linux'] = 'リナックス';
+$lang['openvpn_mac_os'] = 'MacOSの';
+$lang['openvpn_windows'] = 'ウィンドウズ';
