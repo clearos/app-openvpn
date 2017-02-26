507aff98d23502788c25d1711f38ccaf571d3f4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:10:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN app უზრუნველყოფს უსაფრთხო დისტანციური წვდომის ამ სისტემაში და თქვენს ქსელში.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN სერვერზე ამჟამად ავტომატური კონფიგურაცია რეჟიმში. თუ თქვენ შეცვლის თქვენი ქსელის პარამეტრების, OpenVPN იქნება ჭკვიანურად მიიღოს საჭირო ცვლილებები მისი პარამეტრები. თუ გსურთ გამორთოთ ავტომატური კონფიგურაცია, გთხოვთ დააჭირეთ შემდეგ ღილაკს.';
+$lang['openvpn_internet_hostname_tooltip'] = 'ინტერნეტი Hostname გამოიყენება OpenVPN კლიენტის კონფიგურაცია იმ პირობით, რომ ბოლოს მომხმარებლებს. ეს ჰოსტის შეიძლება შეიცვალოს IP პარამეტრები app.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
