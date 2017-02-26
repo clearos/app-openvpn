eff2a2ddd371f3d389f77eb547caada6a28085b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:12:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN app nodrošina drošu attālo piekļuvi šai sistēmai un jūsu vietējā tīklā.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN serveris šobrīd ir auto-konfigurācijas režīmā. Ja jūs mainīt tīkla iestatījumus, OpenVPN būs gudri veikt nepieciešamās izmaiņas tās iestatījumus. Ja vēlaties atslēgt auto-konfigurāciju, lūdzu, noklikšķiniet uz šīs pogas.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internets Hostname tiek izmantots OpenVPN klienta konfigurāciju sniegto gala lietotājiem. Šo hostname var mainīt IP iestatījumus lietotni.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
