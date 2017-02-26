093d2685397a5a49c13502721648e8a3f75f66da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:07:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN的应用程序提供了这个系统和本地网络的安全远程访问。';
+$lang['openvpn_app_name'] = 'OpenVPN的';
+$lang['openvpn_auto_configure_help'] = '目前OpenVPN服务器处于自动配置模式。 如果您更改网络设置，OpenVPN的将智能地做出它的设置进行必要的修改。 如果您想禁用自动配置，请点击以下按钮。';
+$lang['openvpn_internet_hostname_tooltip'] = '互联网主机名被提供给最终用户OpenVPN的客户端配置使用。 该主机可以在IP设置程序进行更改。';
+$lang['openvpn_linux'] = 'Linux的';
+$lang['openvpn_mac_os'] = 'MacOS的';
+$lang['openvpn_windows'] = '视窗';
