f3510d146ca806e7208070565596a4ecc2ee8477
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:10:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'The OpenVPN app veitir örugga fjarlægur aðgangur að þessu kerfi og staðarnetinu.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'The OpenVPN framreiðslumaður er nú í sjálfvirka stillingaskránni ham. Ef þú breytir netstillingum, OpenVPN mun greindur gera nauðsynlegar breytingar á stillingum þess. Ef þú vildi eins og til að slökkva á sjálfvirkri stillingu skaltu smella á eftirfarandi hnapp.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Netið Hýsilheiti er notað í OpenVPN client stillingar veitt notendur. Þetta hostname er hægt að breyta í IP Stillingar forrita.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
