f48a8617c72c0a2f14acfd46a301c827b97ddb65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:11:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN programa suteikia saugią nuotolinę prieigą prie šios sistemos ir jūsų vietinio tinklo.';
+$lang['openvpn_app_name'] = 'Openvpn';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN serveris šiuo metu yra auto-konfigūracijos režimas. Jei pakeisite savo tinklo nustatymus, Openvpn bus protingai padaryti būtinus pakeitimus į savo nustatymus. Jei norite išjungti automatinį konfigūravimą, spustelėkite šį mygtuką.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Interneto kompiuterio vardas yra naudojamas Openvpn kliento konfigūracijos teikiamos galutiniams vartotojams. Tai kompiuterio vardas gali būti pakeistas per TL Nustatymai app.';
+$lang['openvpn_linux'] = '"Linux"';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'langai';
