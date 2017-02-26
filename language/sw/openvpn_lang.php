ca665e658e9962ac8afd5f61b4894727103103d0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:15:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'programu OpenVPN hutoa salama kijijini kupata mfumo huu na mtandao wako wa ndani.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'server OpenVPN ipo katika modi ya auto-Configuration. Kama mabadiliko ya mazingira ya mtandao wako, OpenVPN itakuwa akili kufanya mabadiliko ya lazima kwa mipangilio yake. Kama ungependa afya auto-Configuration, tafadhali bonyeza kifungo zifuatazo.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Jina la mpangishaji Internet ni kutumika katika mteja Configuration OpenVPN zinazotolewa kwa watumiaji wa mwisho. hostname hii inaweza kubadilishwa katika programu IP vipimo.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
