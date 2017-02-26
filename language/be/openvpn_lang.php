ac15fbcf4160bb14fb8f3e58dc86dbe6a0358fac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:06:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Дадатак OpenVPN забяспечвае бяспечны аддалены доступ да гэтай сістэмы і вашай лакальнай сеткі.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Сервер OpenVPN знаходзіцца ў рэжыме аўтаматычнай налады. Калі вы зменіце налады сеткі, OpenVPN будзе разумна ўнесці неабходныя змены ў яго наладах. Калі вы хочаце, каб адключыць функцыю аўтаматычнай налады, калі ласка, націсніце на наступную кнопку.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Інтэрнэт Hostname выкарыстоўваецца ў канфігурацыі кліента OpenVPN, якая прадастаўляецца канчатковым карыстачам. Гэтае імя хаста можна змяніць у наладах прыкладання IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
