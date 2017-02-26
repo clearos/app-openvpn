8734ae12711d2dc698f037a373961b5973e81742
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:17:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN ilova Ushbu tizim va mahalliy tarmoqqa xavfsiz masofadan foydalanish imkonini beradi.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN server avtomatik konfiguratsiya rejimida hozircha. Agar tarmoq sozlamalarini o&#39;zgartirish bo&#39;lsa, OpenVPN ongli uning sozlash uchun zarur o&#39;zgarishlar bo&#39;ladi. Agar avtomatik konfiguratsiyani o&#39;chirish uchun olmoqchi bo&#39;lsangiz, quyidagi tugmani bosing.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internet Hostname oxirgi foydalanuvchilar taqdim OpenVPN mijoz konfiguratsion ishlatiladi. Bu hostname IP Sozlamalar ilova o&#39;zgarishi mumkin.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
