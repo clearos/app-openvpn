b02db1ccdb2eebda21ebd9959cdc04fe4e95ea29
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:09:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'App a OpenVPN bay sekirite aksè a distans nan sistèm sa a ak rezo lokal ou a.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Sèvè a OpenVPN se kounye a nan mòd oto-konfigirasyon. Si ou chanje anviwònman rezo w lan, OpenVPN pral intelijans fè chanjman ki nesesè nan anviwònman li yo. Si ou ta renmen vin enfim oto-konfigirasyon, tanpri klike sou bouton ki anba la a.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Hostname a entènèt yo itilize nan configuration la, te kliyan OpenVPN bay fin divinò. ka hostname sa a dwe chanje nan app a IP Anviwònman.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
