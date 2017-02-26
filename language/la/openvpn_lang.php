47b8ee5a4a47943c75ccd78619469f67b5e6e88a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:11:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'A app OpenVPN providet securus obvius in remotioris ratio huius loci network.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN server est currently in in configuration auto-modum. Si mutare vestri Network occasus, ut intelligenter, ut OpenVPN necessaria mutationes in loca sua. Si vis inactivandi auto- figurationem eius respondeant, placere click in sequenti felis.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Ponitur in figura huius OpenVPN HostnameC Internet users, dum ad finem. Hoc app Occasus IP hostname mutari possit.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Fenestra';
