e7dbc1b863c5ea2f7ac5546f7357c1f527f52501
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:09:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Az OpenVPN alkalmazás biztosítja a biztonságos távoli hozzáférést ezt a rendszert, és a helyi hálózaton.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Az OpenVPN szerver jelenleg automatikus konfigurációs üzemmódban. Ha megváltoztatja a hálózati beállításokat, OpenVPN intelligensen elvégzi a szükséges változtatásokat annak beállításait. Ha le szeretné tiltani az automatikus konfiguráció, kérjük kattintson az alábbi gombra.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Az Internet Hostname használják a OpenVPN kliens konfigurációs nyújtott végfelhasználók számára. Ez hostname lehet változtatni az IP-beállítások alkalmazást.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'ablakok';
