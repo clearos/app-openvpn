3ed3149c7afb34159817bef269404649effeae08
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:17:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Mae&#39;r app OpenVPN yn darparu mynediad o bell diogel i&#39;r system hon ac eich rhwydwaith lleol.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Mae&#39;r gweinydd OpenVPN ar hyn o bryd yn y modd auto-ffurfweddu. Os byddwch yn newid eich gosodiadau rhwydwaith, bydd OpenVPN yn ddeallus gwneud y newidiadau angenrheidiol i&#39;w lleoliadau. Os hoffech chi analluogi auto-ffurfweddu, cliciwch ar y botwm isod.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Mae&#39;r Enw Gwesteiwr Rhyngrwyd yn cael ei ddefnyddio yn y ffurfweddiad cleient OpenVPN ddarperir i defnyddwyr terfynol. Gall hyn enw gwesteiwr yn cael ei newid yn y app Settings IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'ffenestri';
