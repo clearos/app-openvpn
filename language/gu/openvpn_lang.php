fd565320672c609f1024811ecf0f1c9024f19364
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:09:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN એપ્લિકેશન આ સિસ્ટમ અને તમારા સ્થાનીય નેટવર્કમાં સુરક્ષિત દૂરસ્થ વપરાશ પૂરો પાડે છે.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN સર્વર સ્વતઃ રુપરેખાંકનની સ્થિતિ હાલમાં છે. તમે તમારા નેટવર્ક સેટિંગ્સ બદલો તો, OpenVPN intelligently તેના સુયોજનો જરૂરી ફેરફારો કરશે. તમે ઓટો રૂપરેખાંકન નિષ્ક્રિય કરવા માંગો છો, તો નીચેના બટન પર ક્લિક કરો.';
+$lang['openvpn_internet_hostname_tooltip'] = 'ઈન્ટરનેટ યજમાનનામ OpenVPN ક્લાઈન્ટ ઓવરને વપરાશકર્તાઓ માટે પૂરી પાડવામાં રૂપરેખાંકન ઉપયોગ થાય છે. આ યજમાનનામ IP સેટિંગ્સ એપ્લિકેશન માં બદલી શકાય છે.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'વિન્ડોઝ';
