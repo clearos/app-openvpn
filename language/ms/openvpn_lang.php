c6c1afde552c202b39480cb7652a3d66f0374d7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:13:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Aplikasi OpenVPN menyediakan akses jauh selamat untuk sistem ini dan rangkaian tempatan anda.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Pelayan OpenVPN kini dalam mod auto-konfigurasi. Jika anda menukar tetapan rangkaian anda, OpenVPN bijak akan membuat perubahan yang perlu untuk tetapannya. Jika anda ingin untuk melumpuhkan auto-konfigurasi, sila klik pada butang berikut.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internet Nama Hos digunakan dalam konfigurasi klien OpenVPN yang diberikan kepada pengguna akhir. nama hos ini boleh diubah dalam apl Tetapan IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
