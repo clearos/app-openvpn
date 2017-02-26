c6e76998c3db9ac08b0c370af2b7cd72c65242d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:06:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN app bu sistem və yerli şəbəkə təhlükəsiz uzaq erişim təmin edir.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN server avtomatik konfiqurasiya rejimi hazırda. Siz ağ ayarlarını dəyişdirmək, OpenVPN ağıllı onun parametrləri lazımi dəyişikliklər edəcək. avtomatik konfiqurasiya aradan istəyirsinizsə, aşağıdakı &#39;düyməsinə basın.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internet Hostname son istifadəçilər təmin OpenVPN müştəri konfiqurasiya istifadə olunur. Bu kompüter adı IP Settings app dəyişdirilə bilər.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
