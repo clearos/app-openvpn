c36e665628b00e4321335dd9212624aa95c8fc1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:09:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN एप्लिकेशन को इस प्रणाली और अपने स्थानीय नेटवर्क को सुरक्षित दूरस्थ पहुँच प्रदान करता है।';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN सर्वर ऑटो-विन्यास मोड में है। आप अपने नेटवर्क सेटिंग्स बदलते हैं, तो OpenVPN समझदारी से अपनी सेटिंग्स के लिए आवश्यक परिवर्तन कर देगा। आप ऑटो-विन्यास को निष्क्रिय करना चाहते हैं, तो निम्न बटन पर क्लिक करें।';
+$lang['openvpn_internet_hostname_tooltip'] = 'इंटरनेट होस्टनाम OpenVPN ग्राहक अंत उपयोगकर्ताओं के लिए प्रदान विन्यास में प्रयोग किया जाता है। इस होस्टनाम आईपी सेटिंग में बदला जा सकता है।';
+$lang['openvpn_linux'] = 'लिनक्स';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'विंडोज';
