43f255db0b3f88cbc76d8ba1c8c871fca139201d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:14:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'මෙම OpenVPN යෙදුම මෙම පද්ධතිය හා ඔබේ දේශීය ජාලය ආරක්ෂිත දුරස්ථ ප්රවේශය සපයයි.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'මෙම OpenVPN සේවාදායකය ස්වයංක්රීය-වින්යාසය මාදිලිය දැනට. ඔබ ඔබේ ජාල සැකසීම් වෙනස් නම්, OpenVPN බුද්ධිමත්ව එහි සැකසුම් සඳහා අවශ්ය වෙනස්කම් කරනු ඇත. ඔබ ස්වයංක්රීය-වින්යාසය අක්රිය කිරීමට කැමති නම්, පහත දැක්වෙන බොත්තම මත ක්ලික් කරන්න.';
+$lang['openvpn_internet_hostname_tooltip'] = 'අන්තර්ජාලය ධාරක නාමය පරිශීලකයන් හට ලබා දී ඇති OpenVPN සේවාදායකයා වින්යාසය භාවිතා වේ. මෙම සත්කාරක නාමය, IP සැකසුම් යෙදුම තුළ වෙනස් විය හැක.';
+$lang['openvpn_linux'] = 'ලිනක්ස්';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'හි Windows සඳහා';
