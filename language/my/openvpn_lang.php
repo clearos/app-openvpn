597606865ac91a08bcca407c2873b7e348b5a3b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:07:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'အဆိုပါ OpenVPN ကို app ကိုဒီစနစ်နှင့်သင့်ဒေသကွန်ယက်မှလုံခြုံဝေးလံခေါင်သီ access ကိုပေးသည်။';
+$lang['openvpn_app_name'] = 'OpenVPN ကို';
+$lang['openvpn_auto_configure_help'] = 'အဆိုပါ OpenVPN ကို server ကို Auto-configuration ကို mode မှာလက်ရှိဖြစ်ပါတယ်။ သင်သည်သင်၏ကွန်ယက်ကို setting များကိုပြောင်းလဲပစ်လျှင်, OpenVPN ကိုလိမ်မာပါးနပ်သည်၎င်း၏ setting များကိုရန်လိုအပ်သောအပြောင်းအလဲများဖြစ်စေမည်။ သငျသညျ Auto-configuration ကို disable လုပ်ဖို့လိုပါလျှင်အောက်ပါ button ကို click ပါ။';
+$lang['openvpn_internet_hostname_tooltip'] = 'အင်တာနက်ကို hostname အသုံးပြုသူများအားအဆုံးသတ်ရန်ပေးထားသော OpenVPN client ကို configuration ကိုအတွက်အသုံးပြုသည်။ ဒီ hostname ကို IP ကိုချိန်ညှိ app ထဲမှာပြောင်းလဲနိုင်ကြပါတယ်။';
+$lang['openvpn_linux'] = 'Linux ကို';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows ကို';
