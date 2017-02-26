c87d2ba71df67e8cdaa6509aca099c1fe73ae430
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:11:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN을 앱이 시스템과 로컬 네트워크에 안전한 원격 액세스를 제공합니다.';
+$lang['openvpn_app_name'] = 'OpenVPN을';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN을 서버가 자동 설정 모드에서 현재. 네트워크 설정을 변경하면 OpenVPN을 지능적으로 해당 설정에 필요한 사항을 변경합니다. 당신이 자동 구성을 해제하려는 경우, 다음 버튼을 클릭하시기 바랍니다.';
+$lang['openvpn_internet_hostname_tooltip'] = '인터넷 호스트는 최종 사용자에게 제공하는 OpenVPN 클라이언트 구성에서 사용된다. 이 호스트 이름은 IP 설정 앱에서 변경할 수 있습니다.';
+$lang['openvpn_linux'] = '리눅스';
+$lang['openvpn_mac_os'] = '맥 OS';
+$lang['openvpn_windows'] = '윈도우';
