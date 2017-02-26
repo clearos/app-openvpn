fea224d280b48e02aa9217b654eabd261d5a9b7d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:12:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'whakarato te taupānga OpenVPN uru mamao haumaru ki tenei pūnaha me koutou whatunga rohe.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Ko tēnei wā te tūmau OpenVPN i roto i te aratau whirihoranga-aunoa. Ki te huri koe i ō tautuhinga whatunga, ka mohio meinga OpenVPN te huringa tika ki ona tautuhinga. Ki te hiahia koe ki te mono whirihoranga-aunoa, pāwhiria te pātene e whai ake nei.';
+$lang['openvpn_internet_hostname_tooltip'] = 'whakamahia te Pūtaurima Ipurangi kei roto i te whirihoranga kiritaki OpenVPN whakaratohia ki ngā kaiwhakamahi mutunga. Ka taea te huri i tēnei ingoahautū i roto i te taupānga Tautuhinga IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
