68d90a38caedd94bd45c659f225373dfde76915f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:10:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Soláthraíonn an app OpenVPN cianrochtain shlán ar an gcóras seo agus do líonra áitiúil.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Is é an freastalaí OpenVPN i mód uathoibríoch-cumraíochta. Má athraíonn tú do chuid socruithe líonra, beidh OpenVPN dhéanamh intelligently na hathruithe is gá ar a suímh. Más mian leat a chur ar ceal auto-chumraíocht, cliceáil ar an gcnaipe seo a leanas.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Tá an Óstainm Internet úsáidtear sa chumraíocht cliant OpenVPN ar fáil d&#39;úsáideoirí deiridh. Is féidir an óstainm a athrú sa app Socruithe IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
