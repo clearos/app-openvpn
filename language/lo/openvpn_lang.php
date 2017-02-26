c59d78b359247ada9344d98ec7b9fb5feef25f36
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:11:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'ການ app OpenVPN ຫນອງການເຂົ້າເຖິງຫ່າງໄກສອກຫຼີກຮັບປະກັນການລະບົບນີ້ແລະເຄືອຂ່າຍທ້ອງຖິ່ນຂອງທ່ານ.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'ເຄື່ອງແມ່ຂ່າຍຂອງ OpenVPN ປະຈຸບັນແມ່ນຢູ່ໃນຮູບແບບອັດຕະໂນມັດການຕັ້ງຄ່າ. ຖ້າຫາກວ່າທ່ານມີການປ່ຽນແປງການຕັ້ງຄ່າເຄືອຂ່າຍຂອງທ່ານ, OpenVPN ທາງຈະເຮັດໃຫ້ການປ່ຽນແປງທີ່ຈໍາເປັນເພື່ອປັບຄ່າຂອງຕົນ. ຖ້າຫາກວ່າທ່ານຕ້ອງການທີ່ຈະປິດການທໍາງານອັດຕະໂນມັດການຕັ້ງຄ່າ, ກະລຸນາຄລິກໃສ່ປຸ່ມດັ່ງຕໍ່ໄປນີ້.';
+$lang['openvpn_internet_hostname_tooltip'] = 'ອິນເຕີເນັດທີ່ຢູ່ທີ່ຢູ່ທີ່ໃຊ້ໃນການຕັ້ງຄ່າລູກຄ້າ OpenVPN ສະຫນອງໃຫ້ແກ່ຜູ້ຊົມໃຊ້ໃນຕອນທ້າຍ. hostname ນີ້ສາມາດມີການປ່ຽນແປງໃນ app Settings IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
