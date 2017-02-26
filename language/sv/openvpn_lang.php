8d06fcec142d31ed5e1f65705ea83b1e857d300a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:15:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN app ger säker fjärråtkomst till systemet och det lokala nätverket.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN server är närvarande i autokonfigurationsläge. Om du ändrar dina nätverksinställningar, kommer OpenVPN intelligent göra nödvändiga ändringar i inställningarna. Om du vill inaktivera automatisk konfiguration, klicka på följande knapp.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internet Värd används i klientkonfigurations OpenVPN ges till slutanvändare. Detta värdnamn kan ändras i appen IP-inställningar.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'fönster';
