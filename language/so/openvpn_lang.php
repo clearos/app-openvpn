17957fd790bbfd19d712c9f7b97ea7f5961e1b95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:15:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'app OpenVPN wuxuu bixiyaa amaan fog fog si ay nidaam iyo aad network maxaliga ah helaan.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'server OpenVPN ayaa haatan in hab auto-qaabeynta. Haddii aad bedesho goobaha network aad, OpenVPN miyir ka dhigi doonaa isbedel loo baahan yahay in goobaha ay. Haddii aad jeclaan lahayd inaad gab auto-qaabeynta, fadlan riix badhanka soo socda.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Hostname Internet waxaa loo isticmaalaa in qaabeynta macmiilka OpenVPN la siiyo dadka isticmaala dhamaadka. hostname Tani waa la bedeli karaa in app IP Settings ah.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
