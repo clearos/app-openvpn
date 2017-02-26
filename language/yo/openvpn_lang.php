6b183f4b63938e2dae8000ecbb4a35f1c9678d46
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:17:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Awọn OpenVPN app pese aabo latọna wiwọle si yi eto ati ti agbegbe rẹ nẹtiwọki.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'The OpenVPN server wa ni Lọwọlọwọ ni idojukọ-iṣeto ni mode. Ti o ba ti o ba yi nẹtiwọki rẹ eto, OpenVPN yoo intelligently ṣe awọn ayipada si awọn oniwe-eto. Ti o ba yoo fẹ lati mu idojukọ-iṣeto ni, jọwọ tẹ lori awọn wọnyi bọtini.';
+$lang['openvpn_internet_hostname_tooltip'] = 'The Internet Hostname o ti lo ni awọn OpenVPN ni ose iṣeto ni pese si opin awọn olumulo. Yi hostname le wa ni yipada ninu awọn IP Eto app.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
