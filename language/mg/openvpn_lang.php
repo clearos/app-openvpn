966fd3713d7265fbae652e33f4c598a53e7a9aad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:12:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Ny fampiharana OpenVPN manome antoka lavitra ny fidirana ho any amin&#39;ity tontolo ity sy ny eo an-toerana tambajotra.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Ny OpenVPN mpizara amin&#39;izao fotoana izao ao amin&#39;ny Auto-fanahafana fomba. Raha hanova ny tambajotra toe-javatra, dia tamim-pahendrena OpenVPN hanao ny fanovana ilaina ho amin&#39;ny toe-javatra. Raha te-mankarary Auto-fanahafana, azafady tsindrio eo amin&#39;ny bokotra manaraka.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Ny Internet Hostname Ampiasaina ao amin&#39;ny OpenVPN mpanjifa fanahafana omena ny farany mpampiasa. Zavatra hostname dia azo ovaina ao amin&#39;ny IP Settings fampiharana.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
