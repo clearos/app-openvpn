c011f946684a3bf755e8339fe020067824991af8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:16:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Дар барнома OpenVPN дастрасӣ дурдасти эмин ба ин система ва шабакаи маҳаллии шумо.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Сервер OpenVPN аст, айни замон дар ҳолати худкор-конфигуратсия ғайрифаъол месозад. Агар шумо танзимоти шабақаи шумо иваз, OpenVPN intelligently хоҳад тағйироти зарурӣ ба танзимоти он кунад. Агар шумо мехоҳед ба хомӯш худкор-танзимотро таъғир кунед, лутфан ба тугмаи зерин пахш кунед.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Интернет номи мизбони аст, ки дар танзимоти муштарӣ OpenVPN таъмин истифодабарандагони охири истифода бурда мешавад. Ин мизбони мумкин аст, дар барнома Танзимот IP тағйир ёфт.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
