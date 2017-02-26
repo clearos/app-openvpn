b8625d7080755adde43fe7fc11cd783e3e471565
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:13:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Il-app OpenVPN jipprovdi aċċess remot sikur għal din is-sistema u netwerk lokali tiegħek.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Il-server OpenVPN huwa attwalment fil-modalità auto-konfigurazzjoni. Jekk tibdel settings network tiegħek, OpenVPN intelliġenti se tagħmel il-bidliet meħtieġa settings tiegħu. Jekk inti tixtieq li jiskonnettja l-awto-konfigurazzjoni, jekk jogħġbok ikklikkja fuq il-buttuna ta &#39;wara.';
+$lang['openvpn_internet_hostname_tooltip'] = 'L-hostname Internet jintuża fil-konfigurazzjoni klijent OpenVPN pprovduti lill-utenti aħħarin. Dan il hostname tista &#39;tiġi mibdula fil-app PI Settings.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'twieqi';
