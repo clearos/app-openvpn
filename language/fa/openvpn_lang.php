e53117502a657d0c1f7435f0ce0ea0b469918111
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:13:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'برنامه از OpenVPN فراهم می کند دسترسی امن از راه دور به این سیستم و شبکه محلی شما را.';
+$lang['openvpn_app_name'] = 'از OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'سرور OpenVPN حال حاضر در حالت خودکار پیکربندی. اگر شما تنظیمات شبکه خود را تغییر دهید، از OpenVPN هوشمندانه تغییرات لازم به تنظیمات آن است. اگر شما می خواهم برای غیر فعال کردن خودکار پیکربندی، لطفا بر روی دکمه زیر کلیک کنید.';
+$lang['openvpn_internet_hostname_tooltip'] = 'اینترنت نام میزبان در پیکربندی openvpn نیاز مشتری ارائه شده به کاربران نهایی استفاده می شود. این نام میزبان را می توان در برنامه تنظیمات IP را تغییر داد.';
+$lang['openvpn_linux'] = 'لینوکس';
+$lang['openvpn_mac_os'] = 'از MacOS';
+$lang['openvpn_windows'] = 'پنجره ها';
