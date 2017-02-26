58d9b48c8d9f0fe8b3493d3388c0b163f91b412d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:15:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'La aplicación OpenVPN ofrece acceso remoto seguro a este sistema y de la red local.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'El servidor OpenVPN está actualmente en modo de configuración automática. Si cambia la configuración de red, de manera inteligente OpenVPN hacer los cambios necesarios en su configuración. Si desea desactivar la configuración automática, por favor haga clic en el botón siguiente.';
+$lang['openvpn_internet_hostname_tooltip'] = 'El nombre de host de Internet se utiliza en la configuración del cliente OpenVPN proporcionada a los usuarios finales. Este nombre de host se puede cambiar en la aplicación de configuración IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'ventanas';
