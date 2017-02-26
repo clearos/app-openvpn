78944dbc86c83c7c3c10a77baf4b275da74a359d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:12:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN अॅप या प्रणाली आणि आपल्या स्थानिक नेटवर्क सुरक्षित दूरस्थ प्रवेश उपलब्ध आहे.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN सर्व्हर सध्या स्वयं-संरचना मोड मध्ये आहे. आपण आपल्या नेटवर्क सेटिंग्ज बदलू, तर OpenVPN बौद्धिक त्याच्या सेटिंग्जमध्ये आवश्यक ते बदल केले जातील. आपण स्वयं-संरचना अकार्यक्षम इच्छित असल्यास, खालील बटणावर क्लिक करा.';
+$lang['openvpn_internet_hostname_tooltip'] = 'इंटरनेट यजमाननाव शेवटी वापरकर्त्यांना प्रदान OpenVPN क्लायंट कॉन्फिगरेशन वापरले जाते. या यजमाननाव IP सेटिंग्ज अॅप मध्ये बदलले जाऊ शकते.';
+$lang['openvpn_linux'] = 'linux';
+$lang['openvpn_mac_os'] = 'मॅक OS';
+$lang['openvpn_windows'] = 'विंडोज';
