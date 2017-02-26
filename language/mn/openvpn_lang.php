7e0c2500c3b3c4294f2d76a8ab64781d2305ee69
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:12:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN апп энэ систем болон дотоод сүлжээ руу алсаас аюулгүйгээр хандах боломж олгодог.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN сервер автоматаар тохиргоо горимд одоогоор байна. Хэрэв та сүлжээний тохиргоог өөрчлөх бол OpenVPN ухаалгаар түүний тохиргоо шаардлагатай өөрчлөлтүүдийг хийж болно. Хэрэв та автомат тохиргоог идэвхгүй болгохыг хүсэж байгаа бол дараах товчин дээр дарна уу.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Интернэт Hostname эцсийн хэрэглэгчдэд үзүүлж буй OpenVPN үйлчлүүлэгч тохиргоонд ашиглаж байна. Энэ нь хостын нэр IP Тохиргоо програм нь өөрчилж болно.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
