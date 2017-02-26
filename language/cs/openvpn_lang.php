0ab13c5a1b3bae0c7d8a1676b663f64c72a44246
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:07:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN Aplikace poskytuje bezpečný vzdálený přístup k tomuto systému a vaší lokální síti.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Tento server OpenVPN je v současné době v režimu automatické konfigurace. Pokud změníte nastavení sítě, bude OpenVPN inteligentně provést nezbytné změny v jejím nastavení. Pokud byste chtěli zakázat automatickou konfiguraci, klikněte na následující tlačítko.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internet Název počítače se používá v konfiguraci klienta OpenVPN poskytovaných koncovým uživatelům. Tento název hostitele lze změnit v aplikaci Nastavení IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Okna';
