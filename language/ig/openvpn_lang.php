527b6175131a82501d1093d7656532ac1a3fded7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:10:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'The OpenVPN ngwa na-enye nchebe ime ohere a usoro na gị na mpaghara na netwọk.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'The OpenVPN server bụ ugbu a na akpaaka-nhazi mode. Ọ bụrụ na ị gbanwee netwọk gị ntọala, OpenVPN ga amamihe ime mgbanwe ndị na-ya ntọala. Ọ bụrụ na ị ga-achọ iji gbanyụọ auto-nhazi, biko pịa ndị na-esonụ button.';
+$lang['openvpn_internet_hostname_tooltip'] = 'The Internet Hostname na-eji na OpenVPN ahịa nhazi nyere ọgwụgwụ ọrụ. Nke a hostname ga-agbanwe na IP Ntọala ngwa.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
