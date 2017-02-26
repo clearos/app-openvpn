274f26b43c74a34d1bee6cda1dc5c4bdbbf68171
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:17:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'Các ứng dụng OpenVPN cung cấp truy cập từ xa an toàn cho hệ thống này và mạng lưới địa phương của bạn.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Các máy chủ OpenVPN hiện ở chế độ tự động cấu hình. Nếu bạn thay đổi các thiết lập mạng của bạn, OpenVPN sẽ thông minh thực hiện các thay đổi cần thiết để cài đặt. Nếu bạn muốn vô hiệu hóa tính năng tự động cấu hình, bạn hãy click vào nút sau.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internet Hostname được sử dụng trong cấu hình OpenVPN client được cung cấp cho người dùng cuối. tên máy chủ này có thể được thay đổi trong ứng dụng Cài đặt IP.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'các cửa sổ';
