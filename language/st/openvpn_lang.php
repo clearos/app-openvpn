f46a71e8af25318a19fd152015c334261c6824a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:15:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'The app e OpenVPN fana ka boloka ho fihlela e ipatetse batho ba tsamaiso ena le marangrang ea heno.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'Seva OpenVPN hona joale a screen reader mode likoloi-phetolo. Haeba u fetoha di-setting ya netweke ya hao, OpenVPN tla bohlale etsa liphetoho tse hlokahalang ho di-setting tsa eona. Haeba u ka rata ho go tima likoloi-phetolo, ka kōpo tobetsa konopo e latelang.';
+$lang['openvpn_internet_hostname_tooltip'] = 'The Hostname Internet le sebelisitsoe ka ya sedirelwa phetolo OpenVPN fuwa basebedisi bofelo. hostname Sena se ka fetoloa ka app e IP Settings.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
