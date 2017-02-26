a14baf24d8d0b70ae6aff64a94b693589ff8ce49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 20:08:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/openvpn_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['openvpn_app_description'] = 'OpenVPN sovellus tarjoaa turvallisen etäkäytön tämän järjestelmän ja lähiverkossa.';
+$lang['openvpn_app_name'] = 'OpenVPN';
+$lang['openvpn_auto_configure_help'] = 'OpenVPN-palvelin on tällä hetkellä auto-asetustilan. Jos muutat verkkoasetuksia, OpenVPN älykkäästi tehdä tarvittavat muutokset sen asetuksiin. Jos haluat poistaa automaattisen kokoonpanon, klikkaa seuraavaa painiketta.';
+$lang['openvpn_internet_hostname_tooltip'] = 'Internet Hostname käytetään OpenVPN client kokoonpano tarjotaan loppukäyttäjille. Palvelimen nimi voidaan muuttaa IP-asetukset-sovelluksessa.';
+$lang['openvpn_linux'] = 'Linux';
+$lang['openvpn_mac_os'] = 'MacOS';
+$lang['openvpn_windows'] = 'Windows';
